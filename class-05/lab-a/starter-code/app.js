74d39967af93fc5bce2ce2d69e14d2836acf9dd4
renadalkhlafat <renadsalem8888@gmail.com>
2021-06-11 16:04:37 +0300

    add function

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -8,14 +8,14 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
-  let total = a+b;
-  return [total, `The sum of ${a} and ${b} is ${total}.`];
-}
-// console.log( sum(4, 11));
+// function sum(a, b) { //eslint-disable-line
+//   let total = a+b;
+//   return [total, `The sum of ${a} and ${b} is ${total}.`];
+// }
+// // console.log( sum(4, 11));
 
-// Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+// // Here is the test for sum(); uncomment it to run it
+// testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -28,9 +28,9 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
+// function multiply(a, b) { //eslint-disable-line
 
-}
+// }
 
 // Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
@@ -49,9 +49,10 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
+// function sumAndMultiply(a, b, c) { //eslint-disable-line
+  
 
-}
+// }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
 // testSumAndMultiply(4,7,5);
@@ -69,15 +70,38 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
+let total= 0;
+function sum(a) { //eslint-disable-line
+  total += a ;
+}
+  // testSum(4, 7);
+
+
+  let totalMul= 0;
+  function multiply(a) { //eslint-disable-line
+    totalMul *= a ;
+  }
+    // testMultiply(5,9);
+    
+
+
+
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
 
+  for(let i=0; i<sumArr.length; i++){
+    testSum(sumArr[i]);
+    testMultiply(sumArr[i]);
+  }
+  return [total , totalMul, `2,3,4 was passed in as an array of numbers, and ${total} is their sum.`];
 }
+
+
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
