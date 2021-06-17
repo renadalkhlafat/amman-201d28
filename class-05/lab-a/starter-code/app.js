664ff701139f224da258bf704029eba2701a219c
renadalkhlafat <renadsalem8888@gmail.com>
2021-06-17 13:46:24 +0300

    problem 5 solved

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -91,7 +91,7 @@ function sumArray(sumArr) { //eslint-disable-line
 
 // let testArray = [2, 3, 4]; //eslint-disable-line
 
-function sumArray(sumArr) { //eslint-disable-line
+// function sumArray(sumArr) { //eslint-disable-line
 
 //   for(let i=0; i<sumArr.length; i++){
 //     testSum(sumArr[i]);
@@ -117,13 +117,13 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-// function multiplyArray(multArr) { //eslint-disable-line
-//   let total =multiply((multArr[0]*multArr[1]),multArr[2]);
-//   return[total, `The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${total}.`]
-// }
+function multiplyArray(multArr) { //eslint-disable-line
+  let total =multiply((multArr[0]*multArr[1]),multArr[2]);
+  return[total, `The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${total}.`]
+}
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
